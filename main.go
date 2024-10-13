b1b28cf09e48e334bfc30dd56a122d076bdcab2f
DamirPavlik <damir.pavlik@gmail.com>
2024-10-13 18:03:13 +0200

    feat: add main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("port not found")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	v1router := chi.NewRouter()
+	v1router.Get("/healtz", handlerHealthz)
+	router.Mount("/v1", v1router)
+}
