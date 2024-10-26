a65205134d238887d227d3e471b3a4a67873de6c
DamirPavlik <damir.pavlik@gmail.com>
2024-10-26 19:06:37 +0200

    feat: bearer's token into sql queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,4 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email, password, calorie_intake)
-VALUES ($1, $2, $3, $4, $5, $6)
+INSERT INTO users (id, created_at, updated_at, email, password, calorie_intake, bearers_token)
+VALUES ($1, $2, $3, $4, $5, 
+    encode(sha256(random()::text::bytea), 'hex')   
+)
 RETURNING *;
