0e69870ca0e9ad1a6da6da03e8a8a7a8b09beb54
DamirPavlik <damir.pavlik@gmail.com>
2024-10-29 23:02:36 +0100

    feat: add get user by token queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,6 +1,9 @@
 -- name: CreateUser :one
 INSERT INTO users (id, created_at, updated_at, email, password, calorie_intake, bearers_token)
-VALUES ($1, $2, $3, $4, $5, 
+VALUES ($1, $2, $3, $4, $5, $6,
     encode(sha256(random()::text::bytea), 'hex')   
 )
 RETURNING *;
+
+-- name: GetUserByBearers :one
+SELECT * FROM users WHERE bearers_token = $1;
