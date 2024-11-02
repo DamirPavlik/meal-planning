a39582fa87f945428f1010f4ead920684a6eb8fd
DamirPavlik <damir.pavlik@gmail.com>
2024-11-02 21:01:41 +0100

    feat: ingridients squile queries

--- sql/queries/ingridients.sql
+++ sql/queries/ingridients.sql
@@ -2,3 +2,6 @@
 INSERT INTO ingredients (id, created_at, updated_at, calories, name, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetIngridientById :one
+SELECT * FROM ingredients WHERE id = $1;
