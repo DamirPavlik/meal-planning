cf12eabcc9a084ab8a0a7c61bab82f250351e21f
DamirPavlik <damir.pavlik@gmail.com>
2024-11-21 22:53:56 +0100

    refactor: api res

--- json_responder.go
+++ json_responder.go
@@ -7,7 +7,12 @@ import (
 )
 
 func respondWithJSON(w http.ResponseWriter, code int, playload interface{}) {
-	data, err := json.Marshal(playload)
+	response := map[string]interface{}{
+		"code": code,
+		"data": playload,
+	}
+
+	data, err := json.Marshal(response)
 	if err != nil {
 		log.Printf("failed to marshal json repsonse: %v", playload)
 		w.WriteHeader(500)
@@ -22,12 +27,18 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code < 499 {
 		log.Println("res with 5xx error: ", msg)
 	}
-
-	type errResponse struct {
-		Error string `json:"error"`
+	response := map[string]interface{}{
+		"code": code,
+		"error": map[string]interface{}{
+			"msg": msg,
+		},
 	}
-
-	respondWithJSON(w, code, errResponse{
-		Error: msg,
-	})
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("error marshaling json: %v", err)
+		http.Error(w, "Internal Server error", http.StatusInternalServerError)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
 }
